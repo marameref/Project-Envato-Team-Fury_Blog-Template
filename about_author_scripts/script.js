6315b3c5f8c7e161089b78ecc1ebcc99da76306c
emmaxyz90 <44571157+emmaxyz90@users.noreply.github.com>
2020-06-22 00:12:02 +0100

    Add files via upload

---
+++ about_author_scripts/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+	document.getElementById("dropdownNav").style.display = "block";
+	document.getElementById("about").style.marginTop = "250px";
+}
+
+function closeNav() {
+	document.getElementById("dropdownNav").style.display = "none";
+	document.getElementById("about").style.marginTop= "0";
+}
